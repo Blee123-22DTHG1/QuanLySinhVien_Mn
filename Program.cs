b565f9e22bd921d266d834c6e35fbdd3754f6679
Blee123-22DTHG1 <huynhthanhthien4327@gmail.com>
2026-03-28 03:26:55 +0700

    QuanLySinhVien

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using EmployeesManagement.Data;
+using EmployeesManagement.Models;
+using EmployeesManagement.Profiles;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EmployeesManagement.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Kết nối database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Cấu hình Identity với ApplicationUser
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+// Authentication/Authorization
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+// Cấu hình AutoMapper
+var config = new AutoMapper.MapperConfiguration(options =>
+{
+    options.AllowNullDestinationValues = true;
+    options.AllowNullCollections = true;
+    options.AddProfile(new AutomapperProfiles());
+});
+
+var mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
